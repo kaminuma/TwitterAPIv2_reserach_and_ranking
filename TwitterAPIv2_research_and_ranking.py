9ccd7c00a3d038b539d5b7e1ae1a1eab7afd06d6
T.H(kaminuma) <33448874+kaminuma@users.noreply.github.com>
2022-09-19 17:11:54 +0900

    Update TwitterAPIv2_research_and_ranking.py

--- TwitterAPIv2_research_and_ranking.py
+++ TwitterAPIv2_research_and_ranking.py
@@ -1,4 +1,3 @@
-# %%
 import tweepy
 import numpy as np
 import matplotlib.pyplot as plt
@@ -30,7 +29,7 @@ def job():
     
 
     #変数に検索ワード内の調べたいワードを設定
-    # (csv入出力時に0列、0行は見出しになるためダミーを置いた)
+    # (csv入出力時に0列、0行は見出しになるため変数dami(ダミー)を置いた)
 
     dami = str(tweets).count('リオレウス')
     naruga = str(tweets).count('ナルガクルガ')
@@ -49,11 +48,11 @@ def job():
     
     #表示させるリストを別途定めた
     mons_list_hyoji = [naruga, jinouga, tiga, reia, tama, baru, rajan]
-
-
+    
+    #一回の取得での各ワードの出現率をプリント
     print(mons_list_hyoji)
 
-    #csvに'mons.csv'を置いているので読み込みと取得した最新の値を追記
+    #カレントディレクトリに'mons.csv'を置いているので読み込みと取得した最新の値を追記
     with open('mons.csv', 'a',newline='') as f_object:
         writer_object = writer(f_object)
         writer_object.writerow(mons_list)
@@ -63,7 +62,7 @@ def job():
     pd.read_csv('mons.csv')
     df = pd.read_csv('mons.csv')
     
-    #変数dfのそれそれの列をプログラム起動から最新の分まで合計した値をdf_listへ格納し出力
+    #変数dfのそれそれの列を「プログラム起動から最新の分まで」合計した値をdf_listへ格納し出力
     df_list = ([df.iloc[:,1].sum(),df.iloc[:,2].sum(),df.iloc[:,3].sum(),df.iloc[:,4].sum(),df.iloc[:,5].sum(),df.iloc[:,6].sum(),df.iloc[:,7].sum()])
     print(df_list)
     
@@ -82,5 +81,4 @@ while True:
     schedule.run_pending()
     time.sleep(1)
     #1分毎に実行する予定文を無限ループさせ、プログラムを動かし続ける
-
-
+    
